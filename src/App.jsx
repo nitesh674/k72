35b70170303586db959aecdca623acc1bd1eeb0c
nitesh674 <kurmaverma391@gmail.com>
2025-11-21 20:45:29 +0530

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import { useGSAP } from '@gsap/react'
 import { gsap } from 'gsap'
-import { Timeline } from 'gsap/gsap-core.js'
-
-
 
 
 export default function App() {
 
+const stairParentRef = useRef(null);
+
+useGSAP(() => {
+
   const tl = gsap.timeline();
-  useGSAP(function () {
-    tl.from('.stair', {
+
+  tl.set(stairParentRef.current,{ display: "block" }) 
+
+    .from(".stair", {
       height: 0,
       stagger: {
-        amount: -0.30
+        amount: -0.3, 
       }
     })
-    tl.to(".stair", {
-      y: '100%',
-      stagger: {
-        amount: -0.30
-      }
-    })
-  })
 
+    .to(".stair", {
+      y: "100%",
+      stagger: {
+        amount:-0.3,
+      }
+    })
+    .set(stairParentRef.current, { display: "none" });   
+});
 
 
   return (
     <div className='text-white'>
 
-      <div className='w-full h-screen fixed z-20 top-0'>
+      <div ref={stairParentRef} className='w-full h-screen fixed z-20 top-0'>
         <div className='w-full h-full flex'>
           <div className='stair h-full w-1/5 bg-black'></div>
           <div className='stair h-full w-1/5 bg-black'></div>
