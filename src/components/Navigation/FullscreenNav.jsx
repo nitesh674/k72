4f501c0de1e3d83c17a910dfba0503cafbd81bb3
nitesh674 <kurmaverma391@gmail.com>
2025-11-26 15:41:18 +0530

    work on fullscreNav

--- src/components/Navigation/FullscreenNav.jsx
+++ src/components/Navigation/FullscreenNav.jsx
@@ -6,41 +6,41 @@ function FullscreenNav() {
         <div id='FillScreenNav' className='h-screen py-29 w-full absolute  bg-amber-950 text-white'>
             <div className='#'>
                 {/* 1 */}
-                <div className='link border-t-2'>
-                    <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Projets</h1>
-                    <div className='absolute flex'>
-                        <div className='flex overflow-auto items-center'>
+                <div className='relative border-t-2'>
+                    <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Agence</h1>
+                    <div className='absolute text-black bg-[#D3FD50] flex top-0'>
+                        <div className='flex movex items-center'>
                             <h2 className=' whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
-                            <img className='h-20 rounded-full' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
+                            <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
                             <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
-                            <img className='h-20 rounded-full' src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
+                            <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
                         </div>
-                        {/* <div>
-                            <h2>Pour Tout Voir</h2>
-                            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
-                            <h2>Pour Tout Voir</h2>
-                            <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
-                        </div> */}
+                        <div className='flex movex items-center'>
+                            <h2 className=' whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
+                            <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
+                        </div>  
                     </div>
                 </div>
                 {/* 2 */}
-                {/* <div className='link border-t-1'>
-                    <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-3 uppercase text-center'>Agence</h1>
-                    {/* <div>
-                        <div>
-                            <h2>Pour Tout Voir</h2>
-                            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
-                            <h2>Pour Tout Voir</h2>
-                            <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
+                <div className='relative border-t-2'>
+                    <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Projets</h1>
+                    <div className='absolute text-black bg-[#D3FD50] flex top-0'>
+                        <div className='flex overflow-auto items-center'>
+                            <h2 className=' whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
+                            <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
                         </div>
-                         <div>
-                            <h2>Pour Tout Voir</h2>
-                            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
-                            <h2>Pour Tout Voir</h2>
-                            <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
-                        </div>
-                    </div> *
-                </div> */}
+                        <div className='flex overflow-auto items-center'>
+                            <h2 className=' whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
+                            <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
+                        </div>  
+                    </div>
+                </div>
             </div>
         </div>
     )
