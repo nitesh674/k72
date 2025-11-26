8f2e2b517cd78b5e6991427083904d283e2ebbae
nitesh674 <kurmaverma391@gmail.com>
2025-11-26 14:47:10 +0530

    work on fullscreNav

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home.jsx'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import Navbar from './components/Navigation/Navbar.jsx'
+import FullscreenNav from './components/Navigation/FullscreenNav.jsx'
 
 
 
@@ -11,12 +12,13 @@ export default function App() {
 
   return (
     <div>
-      <Navbar/>
-      <Routes>
+      {/* <Navbar/> */}
+      <FullscreenNav/>
+      {/* <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agence' element={<Agence />} />
         <Route path='/projects' element={<Projects />} />
-      </Routes>
+      </Routes> */}
     </div>
   )
 }
--- src/components/Navigation/FullscreenNav.jsx
+++ src/components/Navigation/FullscreenNav.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 
+
 function FullscreenNav() {
     return (
-        <div id='FillScreenNav'>
-            <div id='all-links'>
-                <div className='link'>
-                    <h2>Projects</h2>
-                    <div>
+        <div id='FillScreenNav' className='h-screen py-29 w-full absolute  bg-amber-950 text-white'>
+            <div className=''>
+                {/* 1 */}
+                <div className='link border-t-1'>
+                    <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Projets</h1>
+                     <div className='absolute flex'> 
+                         <div className='flex overflow-auto items-center'>
+                            <h2 className=' whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
+                            <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
+                            <img className='h-20' src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
+                         </div>
+                         {/* <div>
+                            <h2>Pour Tout Voir</h2>
+                            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
+                            <h2>Pour Tout Voir</h2>
+                            <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
+                        </div> */}
+                    </div> 
+                </div>
+                {/* 2 */}
+                {/* <div className='link border-t-1'>
+                    <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-3 uppercase text-center'>Agence</h1>
+                    {/* <div>
                         <div>
                             <h2>Pour Tout Voir</h2>
                             <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
@@ -19,8 +39,8 @@ function FullscreenNav() {
                             <h2>Pour Tout Voir</h2>
                             <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
                         </div>
-                    </div>
-                </div>
+                    </div> *
+                </div> */}
             </div>
         </div>
     )
