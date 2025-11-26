b435e2b026c4bbcb1278045cd4917aac56d330bf
nitesh674 <kurmaverma391@gmail.com>
2025-11-26 14:04:56 +0530

    work on hover effect

---
+++ src/components/Navigation/FullscreenNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function FullscreenNav() {
+    return (
+        <div id='FillScreenNav'>
+            <div id='all-links'>
+                <div className='link'>
+                    <h2>Projects</h2>
+                    <div>
+                        <div>
+                            <h2>Pour Tout Voir</h2>
+                            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
+                            <h2>Pour Tout Voir</h2>
+                            <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
+                        </div>
+                         <div>
+                            <h2>Pour Tout Voir</h2>
+                            <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="image1" />
+                            <h2>Pour Tout Voir</h2>
+                            <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail.jpg?w=640&h=290&s=c3eae0b44f029a1f39a666ffa3c2ca99" alt="image2" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FullscreenNav
