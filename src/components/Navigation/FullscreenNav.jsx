e80ecd4bbc68ed6b87314846c5c4eea72e960249
nitesh674 <kurmaverma391@gmail.com>
2025-11-27 14:46:10 +0530

    work on footer

--- src/components/Navigation/FullscreenNav.jsx
+++ src/components/Navigation/FullscreenNav.jsx
@@ -6,8 +6,8 @@ function FullscreenNav() {
     return (
         <div id='FillScreenNav' className='h-screen py-29 w-full absolute   text-white bg-black'>
             <div className='#'>
-                <div 
-                className='link relative border-t-[0.5px] border-white/50 overflow-hidden'>
+                <div
+                    className='link relative border-t-[0.5px] border-white/50 overflow-hidden'>
                     <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Projets</h1>
                     <div ref={NavSilder} className='MoveLink absolute text-black bg-[#D3FD50] flex top-0 '>
                         <div className='flex movex items-center'>
@@ -21,12 +21,12 @@ function FullscreenNav() {
                             <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
                             <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
                             <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
-                        </div>  
+                        </div>
                     </div>
                 </div>
                 {/* 2 */}
-                 <div 
-                className='link relative border-t-[0.5px] border-white/50 overflow-hidden'>
+                <div
+                    className='link relative border-t-[0.5px] border-white/50 overflow-hidden'>
                     <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Agence</h1>
                     <div ref={NavSilder} className='MoveLink absolute text-black bg-[#D3FD50] flex top-0 '>
                         <div className='flex movex items-center'>
@@ -40,12 +40,12 @@ function FullscreenNav() {
                             <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
                             <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
                             <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
-                        </div>  
+                        </div>
                     </div>
                 </div>
                 {/* 3 */}
-                 <div 
-                className='link relative border-t-[0.5px] border-white/50 overflow-hidden'>
+                <div
+                    className='link relative border-t-[0.5px] border-white/50 overflow-hidden'>
                     <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Contact</h1>
                     <div ref={NavSilder} className='MoveLink absolute text-black bg-[#D3FD50] flex top-0 '>
                         <div className='flex movex items-center'>
@@ -59,12 +59,12 @@ function FullscreenNav() {
                             <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
                             <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
                             <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
-                        </div>  
+                        </div>
                     </div>
                 </div>
                 {/* 4 */}
-                 <div 
-                className='link relative border-y-[0.5px] border-white/50 overflow-hidden'>
+                <div
+                    className='link relative border-y-[0.5px] border-white/50 overflow-hidden'>
                     <h1 className='font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Blogue</h1>
                     <div ref={NavSilder} className='MoveLink absolute text-black bg-[#D3FD50] flex top-0 '>
                         <div className='flex movex items-center'>
@@ -78,9 +78,27 @@ function FullscreenNav() {
                             <img className='h-20 w-50 object-cover  shrink-0 rounded-full' src="/src/assets/image/Fullscreennav1.jpg" alt="image1" />
                             <h2 className='whitespace-nowrap font-[font2] text-[8vw] leading-[0.8] pt-4 uppercase text-center'>Pour Tout Voir</h2>
                             <img className='h-20 w-50 object-cover shrink-0 rounded-full' src="/src/assets/image/Fullscreennav2.jpg" alt="image2" />
-                        </div>  
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className=' footer w-full h-[10vh] bg-amber-300  mt-10  overflow-hidden flex justify-between'>
+
+
+                <div className='w-[30vw] h-[10vh] bg-red-500 flex items-center'>
+                    <div className=' w-[10vw] flex justify-around'>
+                        <h1>frist</h1>
+                        <h1>frist</h1>
+                    </div>
+                </div>
+                <div className='w-[30vw] h-[10vh] bg-red-500 '>
+                    <h1>frist</h1>
+                </div>
+                <div className='w-[30vw] h-[10vh] bg-red-500 '>
+                    <h1>frist</h1>
+                </div>
+
+
             </div>
         </div>
     )
