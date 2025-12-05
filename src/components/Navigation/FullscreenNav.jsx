af6b784b51d4407df77c290c03a411c8a11d0b87
nitesh674 <kurmaverma391@gmail.com>
2025-12-05 11:19:49 +0530

    work on navbar button

--- src/components/Navigation/FullscreenNav.jsx
+++ src/components/Navigation/FullscreenNav.jsx
@@ -12,9 +12,7 @@ function FullscreenNav() {
 
     useGSAP(function () {
         const tl = gsap.timeline()
-        tl.to('#FillScreenNav', {
-            display: 'block'
-        })
+
         tl.from('.stairing', {
             height: 0,
             stagger: {
@@ -31,7 +29,6 @@ function FullscreenNav() {
                 amount: 0.2
             }
         })
-        // tl.pause();
         if (navOpen) {
             gsap.to('.fullscreennav', {
                 display: 'block'
@@ -46,7 +43,7 @@ function FullscreenNav() {
         }
     }, [navOpen])
     return (
-        <div ref={fullScreenRef} id='FillScreenNav' className='fullscreennav  h-screen  overflow-hidden w-full absolute z-50 text-white bg-black'>
+        <div ref={fullScreenRef} id='FillScreenNav' className='fullscreennav  h-screen  overflow-hidden w-full absolute z-50 text-white'>
             <div className='h-screen w-full fixed'>
                 <div className='w-full h-full flex'>
                     <div className='stairing h-full w-1/5 bg-black'></div>
@@ -57,7 +54,7 @@ function FullscreenNav() {
                 </div>
             </div>
             <div ref={fullNavLinkRef} className='relative'>
-                <div className="flex w-full top-0 justify-between items-start px-4 py-2">
+                <div className=" navLink flex w-full top-0 justify-between items-start px-4 py-2">
                     <div className="w-32">
                         <svg className="w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 44">
                             <path fill="white" d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"></path>
