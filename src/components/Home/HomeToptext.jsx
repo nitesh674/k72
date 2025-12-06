e833532aaa8807cc57527ec88e4ecd8931967365
nitesh674 <kurmaverma391@gmail.com>
2025-12-06 20:39:37 +0530

    work on media queray

--- src/components/Home/HomeToptext.jsx
+++ src/components/Home/HomeToptext.jsx
@@ -3,16 +3,16 @@ import Video  from './video'
 
 export default function HomeToptext() {
   return (
-    <div className='font-[font1] pt-5 text-center text-white' >
-      <div className='text-[9vw] uppercase leading-[8vw] flex items-center justify-center '>L'étincelle</div>
-      <div className='text-[9vw] uppercase leading-[8vw] flex items-center justify-center '>
+    <div className=' HomeToptext font-[font1] pt-5 text-center text-white' >
+      <div className='Toptext text-[9vw] uppercase leading-[8vw] flex items-center justify-center '>L'étincelle</div>
+      <div className='Toptext text-[9vw] uppercase leading-[8vw] flex items-center justify-center '>
         qui
-        <div className='h-[7vw] w-[16vw] rounded-full -mt-3 overflow-hidden'>  
+        <div className='Topvideo h-[7vw] w-[16vw] rounded-full -mt-3 overflow-hidden'>  
           <Video/>
         </div>
         génère
         </div>
-      <div className='text-[9vw] uppercase leading-[8vw] flex items-center justify-center '>la créativité</div>
+      <div className='Toptext text-[9vw] uppercase leading-[8vw] flex items-center justify-center '>la créativité</div>
     </div>
   )
 }
