c57b903c9c7449d6d2149de205aadc0bf6cedd63
nitesh674 <kurmaverma391@gmail.com>
2025-12-07 10:50:46 +0530

    work on media queray

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 export default function HomeBottomText() {
   return (
-    <div className='font-[font1]  flex justify-center gap-2 text-white'>
-        <div className="border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] border-white rounded-full px-12 uppercase ">  
-        <Link className='text-[5vw] leading-[5vw]  ' to='/Projects'>Projects</Link>
-        </div>
-        <div className="border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] border-white rounded-full px-12   uppercase ">  
-        <Link className='text-[5vw] leading-[5vw]' to='/Agence'>Agences</Link>
-        </div>
-    </div>
+      <div className='BottomText font-[font1]  flex justify-center gap-2 text-white'>
+          <div className="border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] border-white rounded-full px-12 uppercase ">  
+          <Link className='text-[5vw] leading-[5vw]  ' to='/Projects'>Projects</Link>
+          </div>
+          <div className="border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] border-white rounded-full px-12   uppercase ">  
+          <Link className='text-[5vw] leading-[5vw]' to='/Agence'>Agences</Link>
+          </div>
+      </div>
   )
 }
