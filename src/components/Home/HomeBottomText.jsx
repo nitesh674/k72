a5bc0d61db2e892d938d07c1ed93b0d767b0ab76
nitesh674 <kurmaverma391@gmail.com>
2025-12-08 13:49:23 +0530

    work on media queray

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 export default function HomeBottomText() {
   return (
       <div className='BottomText font-[font1]  flex justify-center gap-2 text-white'>
-          <div className="border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] border-white rounded-full px-12 uppercase ">  
-          <Link className='text-[5vw] leading-[5vw]  ' to='/Projects'>Projects</Link>
+          <div className="Innerdiv border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] md:text-[5vw] md:leading-[5vw] border-white rounded-full px-12 uppercase ">  
+          <Link className='text-[5vw] leading-[5vw] md:text-[5vw] md:leading-[5vw]  ' to='/Projects'>Projects</Link>
           </div>
-          <div className="border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] border-white rounded-full px-12   uppercase ">  
-          <Link className='text-[5vw] leading-[5vw]' to='/Agence'>Agences</Link>
+          <div className="Innerdiv border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] md:text-[5vw] md:leading-[5vw] border-white rounded-full px-12   uppercase ">  
+          <Link className='text-[5vw] leading-[5vw] md:text-[5vw] md:leading-[5vw] ' to='/Agence'>Agences</Link>
           </div>
       </div>
   )
--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
 
   return (
     <div className='flex justify-between fixed top-0 w-full z-4 '>
-      <div className='w-34 p-2'>
+      <div className='logo w-34 p-2'>
         <svg className='w-full' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 44">
           <path fill='white' fillRule="evenodd" d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"></path>
         </svg>
@@ -24,7 +24,7 @@ function Navbar() {
           navGreenRef.current.style.height = "0%"
           lineRef.current.style.color = "white";
         }}
-        className='w-55 h-13 relative  bg-black  overflow-hidden'>
+        className='rightNav w-55 h-13 relative bg-black  overflow-hidden'>
         <div  ref={lineRef} className='relative flex flex-col justify-center items-end gap-1  h-full px-8 z-20' >
           <div className='w-15 h-0.5  bg-amber-50'></div>
           <div className='w-8 h-0.5 bg-amber-50'></div>
