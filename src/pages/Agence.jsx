953147dfaec001dba165792c56a9c3da55afd336
nitesh674 <kurmaverma391@gmail.com>
2025-12-09 11:55:13 +0530

    work on media queray

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -49,15 +49,15 @@ export default function Agence() {
   return (
     <div >
       <div className='section1 relative  py-1 '>
-      <div ref={imageDevRef} className=' h-[20vw] w-[15vw] rounded-3xl absolute top-[30vh] left-105 overflow-hidden'>
+      <div ref={imageDevRef} className='scrolldev h-[20vw] w-[15vw] rounded-3xl absolute top-[30vh] left-105 overflow-hidden'>
             <img  ref={imageRef} className='h-full object-cover ' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="#" />
       </div>
       <div className=' relative font-[font2]'>
-      <div className='mt-[55vh]'>
+      <div className='agenceText mt-[55vh]'>
       <h1 className='text-[19vw] text-center leading-[17vw] tracking-wide text-black uppercase'>Soixan7e <br />
         Douze</h1>
       </div>
-      <div className='pl-[40%]' > 
+      <div className='AgencePra pl-[40%]' > 
         <p className='text-5xl text-black'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
       </div>
     </div>
