8b441521a61f56048836b8648a355cba316e209c
nitesh674 <kurmaverma391@gmail.com>
2025-12-10 10:21:51 +0530

    work on projets page

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -5,7 +5,7 @@ export default function HomeBottomText() {
   return (
       <div className='BottomText font-[font1]  flex justify-center gap-2 text-white'>
           <div className="Innerdiv border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] md:text-[5vw] md:leading-[5vw] border-white rounded-full px-12 uppercase ">  
-          <Link className='text-[5vw] leading-[5vw] md:text-[5vw] md:leading-[5vw]  ' to='/Projects'>Projects</Link>
+          <Link className='text-[5vw] leading-[5vw] md:text-[5vw] md:leading-[5vw]  ' to='/Projects'>Projets</Link>
           </div>
           <div className="Innerdiv border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] md:text-[5vw] md:leading-[5vw] border-white rounded-full px-12   uppercase ">  
           <Link className='text-[5vw] leading-[5vw] md:text-[5vw] md:leading-[5vw] ' to='/Agence'>Agences</Link>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div>
-      <h1 className='text-6xl'>Projects page</h1> 
+    <div className='p-4'>
+        <div className='bg-amber-300 pt-[40vh]'>
+            <h1 className='font-[font2] text-[15vw] uppercase'>Projets</h1> 
+        </div>
+        <div>
+          <div className='w-full h-[400vh] flex gap-4 bg-amber-950 -mt-20'>
+               <div className='w-1/2 h-full bg-amber-50'></div>
+               <div className='w-1/2 h-full bg-amber-400'></div>
+          </div>
+        </div>
     </div>
   )
 }
