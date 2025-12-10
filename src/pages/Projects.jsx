ac3e59aaa860a06d220af0b35296ffd5d8400a43
nitesh674 <kurmaverma391@gmail.com>
2025-12-10 11:47:56 +0530

    work on projets page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import ProjectsCard from '../components/Projects/ProjectsCard'
 
 export default function Projects() {
+  const images = [{
+    image1: 'https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail.jpg?w=1280&h=960&s=650a04dfc31ad85bfc64c0ddccc83f1e',
+    image2:'https://k72.ca/images/caseStudies/OKA/OKA_thumbnail.jpg?w=1280&h=960&s=c12c27c9db3c521e4c82a246a8d5c022',
+  },{
+    image1:'https://k72.ca/images/caseStudies/Opto_Reseau_Brand/opto_thumbnail2.jpg?w=1280&h=960&s=7f23d7d824eb04c784dcf51380fe3996',
+    image2:'https://k72.ca/images/caseStudies/SHELTON/thumbnailimage_shelton.jpg?w=1280&h=960&s=63d0eaa180cbc02d3ada285ad9ef1479',
+  },{
+    image1:'https://k72.ca/images/caseStudies/COUP_FUMANT/CF_thumbnail.jpg?w=1280&h=960&s=c119303a20520c4188aa3f592038fd4c',
+    image2:'https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b'
+  }
+  ]
   return (
     <div className='p-4'>
-        <div className=' pt-[40vh]'>
-            <h1 className='font-[font2] text-[15vw] uppercase'>Projets</h1> 
-        </div>
-        <div className='-mt-20'>
-          <div className='w-full h-[400px] mb-4 flex gap-4 '>
-               <div className='w-1/2 h-full relative transition-all rounded-none hover:rounded-[50px] overflow-hidden bg-amber-700'>
-                  <img className='w-full h-full object-cover' src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b" alt="" />
-                  <div className='absolute top-0 left-0  w-full h-full  bg-black/10'>
-                    <h2 className='font-[font2] text-[4vw] uppercase text-white border-0' >Vior Le Projet</h2>
-                  </div>
-               </div>
-          </div>
-        </div>
+      <div className=' pt-[40vh]'>
+        <h1 className='font-[font2] text-[15vw] uppercase'>Projets</h1>
+      </div>
+        {images.map(function(elem){
+          return <ProjectsCard iamge1={elem.image1} iamge2={elem.image2} />;
+        })}
     </div>
   )
 }
